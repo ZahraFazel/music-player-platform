f863f00546313202626e98794383a0ae1c90415b
atenasadat <atenasaghi@yahoo.com>
2021-07-24 22:04:48 +0430

    create music front and back added

--- musicplayer_app/urls.py
+++ musicplayer_app/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'new_playlist/', views.add_playlist),
+    url(r'add_music/', views.add_music , name='add_music'),
+    url(r'add_playlist/', views.add_playlist),
     url(r'musicplayer_app/', views.index),
 ]
--- musicplayer_app/views.py
+++ musicplayer_app/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.http.response import HttpResponse
 
 from django.shortcuts import render
-
+from  .models import Music
 
 # Create your views here.
 def index(request):
@@ -12,3 +12,18 @@ def index(request):
 
 def add_playlist(request):
     return render(request, 'musicplayer_app/add_playlist.html')
+
+
+def add_music(request):
+
+    if request.method == "POST":
+        music_name  = request.POST.get('musicname')
+        music_artist = request.POST.get('artist')
+        music_release_date = request.POST.get('release_date')
+
+        # m = Music(artist=music_artist,name= music_name,release_date=music_release_date,num_stars=0)
+        # m.save()
+
+
+    return  render(request , 'musicplayer_app/add_music.html/')
+
